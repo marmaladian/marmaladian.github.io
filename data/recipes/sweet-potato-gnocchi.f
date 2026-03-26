291fb375890bb8bec3f457a9d82fc7317f6c9c52
marmaladian <91722632+marmaladian@users.noreply.github.com>
2026-03-26 20:31:15 +1100

    Add sweet potato gnocchi recipe, update running log

---
+++ data/recipes/sweet-potato-gnocchi.f
@@ -0,0 +1,24 @@
+: title: Sweet potato gnocchi
+
+2 Gnocchi
+* 2 large sweet potatoes
+* plain flour
+* 1-2 tsp salt
+
+  Roast the sweet potatoes (230 degrees celsius) for around 90 minutes.
+  Allow to cool and place in the fridge overnight (cover or wrap in foil).
+  Remove the skins and mash until smooth. Add flour gradually and mix until a loose dough is formed - just strong enough to hold its shape when you roll a handful into snakes, but will break if you lift up one end.
+  Cover the bench with flour, then roll all the dough into snakes (about 2cm thick) and cut into 2-3cm lengths. Optionally, you can roll these into balls and/or press with a fork to create grooves.
+  You can coat the gnocchi lightly with flour and put them into a bag in a single layer and freeze them. To cook, place in rapidly boiling salted water and remove when the gnocchi float to the surface.
+
+2 Creamy tomato and chilli sauce
+* 1 tbsp olive oil
+* 2-3 cloves garlic chopped
+* 2 tsp chilli flakes
+* 400g diced tomatoes
+* 2-3 tbsp tomato paste (I think I used a garlic, onion and herbs one)
+* 1 tsp dried oregano
+* 200ml oat milk (less if not using cheese)
+* handful each of vegan mozzarella and cheddar
+
+  Heat oil then add the garlic and chilli. Cook for 1-2 minutes then add the diced tomatoes, then the tomato paste and oregano. Cook for a few minutes then add the oat milk, then the cheeses. Cook until thickened slightly, season to taste and then add the drained gnocchi.
